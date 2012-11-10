ee5b4eae906dab95adb35800d09a00e2316789b3
fairwinds <fairwinds.dp@gmail.com>
2012-11-10 15:23:09 -0400

    apidoc product name changes

--- apidoc/docgen.py
+++ apidoc/docgen.py
@@ -31,7 +31,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# parse out Titanium API documentation templates into a 
+# parse out TideSDK API documentation templates into a 
 # format that can be used by other documentation generators
 # such as PDF, etc.
 
