ed663cd9688d5cecfe334b0fe8b008bf2a3a4778
Steven Verbeek <dubcanada@gmail.com>
2012-11-10 23:25:03 -0400

    Added ifdef around ssl tcp socket edits for Mac, revert this commit if it works in windows & linux

--- src/modules/socket/ssl_tcp_socket_binding.cpp
+++ src/modules/socket/ssl_tcp_socket_binding.cpp
@@ -24,7 +24,12 @@
 namespace ti
 {
 	SecureTCPSocket::SecureTCPSocket(Host *host, TCPSocketBinding * tcp_socket_binding)
-		: Socket<boost::asio::ssl::stream<tcp::socket&> >(host, string("Socket.SecureTCPSocket")),
+// TODO: See if this breaks windows and linux, if it doesn't remove this.
+#ifdef OS_OSX
+        : Socket<boost::asio::ssl::stream<tcp::socket&> >(host, string("Socket.SecureTCPSocket")),
+#else
+        : Socket(host, string("Socket.SecureTCPSocket")),
+#endif
 		ctx(*SocketService::getIOService(), boost::asio::ssl::context::sslv23),
 		tcp_socket(NULL)
 	{
