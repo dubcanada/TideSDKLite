394cb563ab50ebc5b0ce74a637b8267e7e34f37c
Mital Vora <mital.d.vora@gmail.com>
2012-11-28 17:16:31 +0530

    Moving the utils directory copy logic locally to lib/utils - to be removed with poco, updating license on the files which I have modified.

--- site_scons/tide.py
+++ site_scons/tide.py
@@ -141,23 +141,6 @@ class BuildConfig(object):
         self.tide_include_dir = path.join(self.dir, 'sdk', 'include')
         self.tide_utils_dir = path.join(self.tide_source_dir, 'lib', 'utils');
 
-    # Get a separate copy of the Kroll Utils for a particular build piece
-    # Give: A unique directory for that build piece where the utils should be copied
-    def get_tide_utils(self, dir, unzip=True):
-        effess.copy_to_dir(self.tide_utils_dir, dir)
-        sources = Glob('%s/utils/*.cpp' % dir) + \
-            Glob('%s/utils/poco/*.cpp' % dir) + \
-            Glob('%s/utils/%s/*.cpp' % (dir, self.os))
-        if self.is_win32() and unzip:
-            sources.extend(Glob('%s/utils/unzip/*.cpp' % dir))
-        if self.is_win32():
-            sources.extend(Glob('%s/utils/win/*.cpp' % dir))
-        if self.is_osx():
-            sources.extend(Glob('%s/utils/%s/*.mm' % (dir, self.os)))
-        if self.is_osx() or self.is_linux():
-            sources.extend(Glob('%s/utils/posix/*.cpp' % dir))
-        return sources
-
     def init_os_arch(self):
         if self.is_linux() and self.is_64():
             self.env.Append(CPPFLAGS=['-m64', '-Wall', '-Werror','-fno-common', '-fvisibility=hidden', '-fno-strict-aliasing'])
